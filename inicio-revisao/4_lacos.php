05e6cbf5cf7134546360b7b0f0d0749d3ee864dd
Dan2401e <tpsdan4@gmail.com>
2026-08-10 14:42:17 -0300

    lacos

---
+++ inicio-revisao/4_lacos.php
@@ -0,0 +1,23 @@
+<?php
+
+// Laço FOR - Exemplo alicado em Tabuada
+for ($i = 1; $i <= 10; $i++) {
+    echo "8 x $i = " . (8 * $i) . "<br>";
+}
+
+// While - (Enquanto) Contagem regressiva
+echo "<br>";
+$n = 10;
+while ($n >= 0) {
+    echo $n . "<br>";
+    $n--;
+}
+
+// Do While - (Faça enquanto) Executa ao menos 1 vez
+
+echo "<br>";
+$j = 10;
+do {
+    echo "j vale: $j <br>";
+    $j--;
+} while ($j >= 0);
